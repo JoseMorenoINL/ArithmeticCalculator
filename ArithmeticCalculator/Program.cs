8ea6fdd61690ea496874ea182891af18324c16f3
josemoreno.inl <josemoreno.inl@outlook.com>
2017-09-13 14:04:07 -0400

    First Commit

---
+++ ArithmeticCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArithmeticCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
